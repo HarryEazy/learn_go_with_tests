d7a30582b7a2805be5263b142ec20c777c5f235f
HarryEazy <harrycamps@me.com>
2024-05-17 19:27:32 +0100

    all test working, hello world chapter complete

--- hello_world/hello_world.go
+++ hello_world/hello_world.go
@@ -1,17 +1,36 @@
 package main
 
 import "fmt"
-const englishHelloPrefix = "Hello, "
 
-func Hello(name string) string {
+const (
+	spanish = "Spanish"
+	french  = "French"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
 }
 
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
 
 func main() {
-	fmt.Println(Hello("Kaza"))
+	fmt.Println(Hello("", ""))
 }
-
