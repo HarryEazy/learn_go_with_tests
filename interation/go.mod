da4866c47f9f3dcf43f0f5a37fd26be9e887fc51
HarryEazy <harrycamps@me.com>
2024-05-17 20:43:37 +0100

    Iteration chapter complete, all tests working

---
+++ interation/go.mod
@@ -0,0 +1,3 @@
+module iterationz
+
+go 1.22.3
---
+++ interation/iteration.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+const repeatCount = 5
+
+func Repeat(character string) (repeated string) {
+	
+	for i := 0; i < repeatCount; i++ {
+		repeated += character
+	} 
+	return
+}
+
+func main() {
+	fmt.Println("Running.... ..")
+}
